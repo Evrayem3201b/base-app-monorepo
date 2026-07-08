d8438275cf68a564f6aeddb0e847d53aea154d06
Evrayem3201b <betecno100@gmail.com>
2026-07-08 22:39:50 +0300

    made api vercel/local compatible

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,33 +1,14 @@
 import "dotenv/config";
-import { Hono } from "hono";
-import helloRoute from "./routes/hello.js";
-import { auth } from "./lib/auth.js";
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
-import { secureHeaders } from "hono/secure-headers";
-import { config } from "./lib/config.js";
-import { requireAuth } from "./middleware/auth.js";
 
-const app = new Hono()
-.use("/*", logger())
-.use("/*", secureHeaders())
-.use("/*", cors({ origin: config.allowedOrigins, credentials: true }))
-  .get("/api/health", (c) => c.json({ status: "ok" }))
-  .get("/api/user",requireAuth(), (c) => {
-	const user = c.get("user")
-	
-	if(!user) return c.body(null, 401);
-  	return c.json({
-	  user
-	})
-  })
-  .route("/api/hello", helloRoute)
-  .on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw))
-  .onError((err, c) => {
-  console.error(err);
-  return c.json({ error: "Internal Server Error" }, 500);
-  })
-  .notFound((c) => c.json({ error: "Not Found" }, 404));
+import { serve } from "@hono/node-server";
+import app from "./app.js";
 
-export type AppType = typeof app;
-export default app;
+serve(
+  {
+    fetch: app.fetch,
+    port: Number(process.env.PORT ?? 3000),
+  },
+  (info) => {
+    console.log(`Server running on http://localhost:${info.port}`);
+  }
+);
